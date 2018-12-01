c2a71acecd96e6c795bc140e8022a6d4896e0855
Calum Gilchrist <calum@homelinen.org>
2018-12-01 18:26:09 +0000

    Looping Freq

--- 1/frequency_test.go
+++ 1/frequency_test.go
@@ -4,11 +4,6 @@ import (
 	"testing"
 )
 
-// Strings containing addition
-
-// Strings containing subtraction
-
-// Combined samle
 func TestFrequency(t *testing.T) {
 
 	sample := []string{
@@ -36,6 +31,25 @@ func TestFrequency(t *testing.T) {
 	result = 0
 
 	output = Frequency(sample)
+	if output != result {
+		t.Errorf("Parsing of sample is incorrect, got: %d, want: %d.", output, result)
+	}
+}
+
+func TestFrequencyTwice(t *testing.T) {
+
+	sample := []string{
+		"+3",
+		"+3",
+		"+4",
+		"-2",
+		"-4",
+	}
+
+	var result int64
+	result = 10
+
+	output := FrequencyTwice(sample)
 	if output != result {
 		t.Errorf("Parsing of sample is incorrect, got: %d, want: %d.", output, result)
 	}
