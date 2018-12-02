96c4e0730280bcd16d277977e8080695c40a8870
Calum Gilchrist <calum@homelinen.org>
2018-12-02 22:14:04 +0000

    First Pass Implementation

--- 2/boxsum.go
+++ 2/boxsum.go
@@ -2,8 +2,28 @@ package main
 
 import "fmt"
 
-func ExactlyTwo(boxid string) bool {
-	return false
+func ExactlyTwiceOrThrice(boxid string) (bool, bool) {
+
+	var twice, thrice bool
+	letter_counts := make(map[rune]int)
+
+	for _, letter := range boxid {
+		if letter_counts[letter] != 0 {
+			letter_counts[letter] += 1
+		} else {
+			letter_counts[letter] = 1
+		}
+	}
+
+	for _, v := range letter_counts {
+		if v == 2 {
+			twice = true
+		} else if v == 3 {
+			thrice = true
+		}
+	}
+
+	return twice, thrice
 }
 
 func ExactlyThree(boxid string) bool {
--- 2/boxsum_test.go
+++ 2/boxsum_test.go
@@ -4,19 +4,32 @@ import (
 	"testing"
 )
 
-func TestExactlyTwo(t *testing.T) {
+func TestExactlyTwiceOrThrice(t *testing.T) {
 
-	output := ExactlyTwo("bababc")
-	result := true
-	if output != result {
-		t.Errorf("Parsing of sample is incorrect, got: %v, want: %v.", output, result)
+	test_set := []struct {
+		id         string
+		exp_twice  bool
+		exp_thrice bool
+	}{
+		{"bababc", true, true},
+		{"abbcde", true, false},
+		{"abcccd", false, true},
+		{"aabcdd", true, false},
+		{"ababab", false, true},
+		{"aaaaba", false, false},
 	}
-}
-func TestExactlyThree(t *testing.T) {
 
-	output := ExactlyThree("bababc")
-	result := true
-	if output != result {
-		t.Errorf("Parsing of sample is incorrect, got: %v, want: %v.", output, result)
+	for _, testrow := range test_set {
+		res_twice, res_thrice := ExactlyTwiceOrThrice(testrow.id)
+		exp_twice := testrow.exp_twice
+		exp_thrice := testrow.exp_thrice
+
+		if res_twice != exp_twice {
+			t.Errorf("Parsing of sample is incorrect, got: %v, want: %v.", res_twice, exp_twice)
+		}
+
+		if res_thrice != exp_thrice {
+			t.Errorf("Parsing of sample is incorrect, got: %v, want: %v.", res_thrice, exp_thrice)
+		}
 	}
 }
