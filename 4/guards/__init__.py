f6631f3c9e2c68763d8f3c82b4415fa953c31bbc
Calum Gilchrist <calum@homelinen.org>
2018-12-09 18:59:35 +0000

    Parse Through Input

--- 4/guards/__init__.py
+++ 4/guards/__init__.py
@@ -1,21 +1,66 @@
 
+from datetime import datetime
+import re
+
 class GuardSleep():
     guard = 0
-    date = ""
     duration = 0
-    start_minute = 0
+    date = None
+    slept = False
 
-    def __init__(self, guard, date, start_minute, duration=0):
+    def __init__(self, guard, date=datetime.today(), duration=0):
         self.guard = guard
-        self.date = date
         self.duration = duration
+        self.date = date
 
-    def wakes(self, minute):
-        self.duration = minute - self.start_minute 
+    def wakes(self, date):
+        self.duration = parse_date(date).minute - self.date.minute
+
+    def sleep(self, date):
+        self.date = parse_date(date)
+        self.slept = True
+
+    def __str__(self):
+        return f'{self.guard} {self.date} {self.duration}'
+
+    def __repr__(self):
+        return f'GuardSleep({self.guard}, {self.date}, {self.duration}, {self.slept})'
+
+    def __eq__(self, other):
+        return int(self.guard) == int(other.guard) and self.date == other.date and self.duration == other.duration
+
+
+def parse_date(datestr):
+    date = datetime.strptime(datestr, '%Y-%m-%d %H:%M')
+    return date
 
 
 def parse_sorted(text_input):
-    return []
+
+    guards_sleep = []
+    guard_sleep = None
+    for line in text_input.split("\n"):
+        match = re.search(r'\[(\d+-\d+-\d+ \d+:\d+)] (.*)$', line)
+        date = match[1]
+        action = match[2]
+
+        if "Guard" in action:
+            guard = re.search(r'#(\d+)', action)
+            guard_sleep = GuardSleep(guard[1])
+            guards_sleep.append(guard_sleep)
+
+        if "falls asleep" in action and guard_sleep.slept:
+            guard_sleep = GuardSleep(guard_sleep.guard)
+            guards_sleep.append(guard_sleep)
+
+        if "falls asleep" in action:
+            guard_sleep.sleep(date)
+        if "wakes up" in action:
+            guard_sleep.wakes(date)
+
+        # print(f"line: {line}\n{guard_sleep}")
+
+    return guards_sleep
 
 def find_most_sleepy(guards_asleep):
     return 0
--- 4/tests/guards_test.py
+++ 4/tests/guards_test.py
@@ -1,4 +1,4 @@
-from guards import GuardSleep, parse_sorted, find_most_sleepy, find_most_sleepy_minute
+from guards import GuardSleep, parse_sorted, find_most_sleepy, find_most_sleepy_minute, parse_date
 
 def test_parse_input():
 
@@ -21,26 +21,31 @@ def test_parse_input():
         [1518-11-05 00:03] Guard #99 begins shift
         [1518-11-05 00:45] falls asleep
         [1518-11-05 00:55] wakes up
-    """
+    """.strip()
+    
 
     guards_asleep = [
-                GuardSleep(10, "1518-11-01", 5, 20),
-                GuardSleep(99, "1518-11-02", 40, 10),
-                GuardSleep(10, "1518-11-03", 24, 5),
-                GuardSleep(99, "1518-11-04", 36, 10),
-                GuardSleep(99, "1518-11-05", 45, 10),
-                ]
+            GuardSleep(10, parse_date("1518-11-01 00:05"), 20),
+            GuardSleep(10, parse_date("1518-11-01 00:30"), 25),
+            GuardSleep(99, parse_date("1518-11-02 00:40"), 10),
+            GuardSleep(10, parse_date("1518-11-03 00:24"), 5),
+            GuardSleep(99, parse_date("1518-11-04 00:36"), 10),
+            GuardSleep(99, parse_date("1518-11-05 00:45"), 10),
+            ]
+    print(parse_sorted(text_input))
 
-    assert guards_asleep == parse_sorted(text_input)
+    assert parse_sorted(text_input)[0] == guards_asleep[0]
+    assert parse_sorted(text_input) == guards_asleep
     
 
 def test_find_most_sleep():
     guards_asleep = [
-                GuardSleep(10, "1518-11-01", 5, 20),
-                GuardSleep(99, "1518-11-02", 40, 10),
-                GuardSleep(10, "1518-11-03", 24, 5),
-                GuardSleep(99, "1518-11-04", 36, 10),
-                GuardSleep(99, "1518-11-05", 45, 10),
+            GuardSleep(10, parse_date("1518-11-01 00:05"), 20),
+            GuardSleep(10, parse_date("1518-11-01 00:30"), 25),
+            GuardSleep(99, parse_date("1518-11-02 00:40"), 10),
+            GuardSleep(10, parse_date("1518-11-03 00:24"), 5),
+            GuardSleep(99, parse_date("1518-11-04 00:36"), 10),
+            GuardSleep(99, parse_date("1518-11-05 00:45"), 10),
                 ]
 
     most_asleep = 10
@@ -49,11 +54,12 @@ def test_find_most_sleep():
 
 def test_find_sleepiest_minute():
     guards_asleep = [
-                GuardSleep(10, "1518-11-01", 5, 20),
-                GuardSleep(99, "1518-11-02", 40, 10),
-                GuardSleep(10, "1518-11-03", 24, 5),
-                GuardSleep(99, "1518-11-04", 36, 10),
-                GuardSleep(99, "1518-11-05", 45, 10),
+            GuardSleep(10, parse_date("1518-11-01 00:05"), 20),
+            GuardSleep(10, parse_date("1518-11-01 00:30"), 25),
+            GuardSleep(99, parse_date("1518-11-02 00:40"), 10),
+            GuardSleep(10, parse_date("1518-11-03 00:24"), 5),
+            GuardSleep(99, parse_date("1518-11-04 00:36"), 10),
+            GuardSleep(99, parse_date("1518-11-05 00:45"), 10),
                 ]
 
     most_sleepy_minute = 24
